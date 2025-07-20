c42799890bb5f60737ee896541beeddf3a60192f
unknown <theoladejitoluwa@gmail.com>
2025-07-20 11:51:51 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function App() {
+const randomColor =() => {
+  const charactes = "0123456789abcdef"
+}
+
+  return (
+    <>
+      <h1>Background Color : {randomColor}</h1>
+      <button>Change Color</button>
+    </>
+  );
+}
+
+export default App;
