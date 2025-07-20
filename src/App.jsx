85390cf3f710562889299a07cf1a903c73bce415
unknown <theoladejitoluwa@gmail.com>
2025-07-20 12:24:08 +0100

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
 
 function App() {
-const randomColor =() => {
-  const charactes = "0123456789abcdef"
-}
+  const [color, setColor] = useState("#000000");
+
+  const randomColor = () => {
+    const characters = "0123456789abcdef";
+    let value = "#";
+    for (let i = 0; i < 6; i++) {
+      value += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    setColor(value);
+    console.log(value);
+  };
+
+  let textColor = color === "#000000" ? "#ffffff" : "#000000";
 
   return (
-    <>
-      <h1>Background Color : {randomColor}</h1>
-      <button>Change Color</button>
-    </>
+    <div style={{ backgroundColor: color, minHeight: "100vh" }}>
+      <>
+        <h1 style={{ color: textColor }}> Background Color :{color} </h1>
+        <button onClick={randomColor}>Change Color</button>
+      </>
+    </div>
   );
 }
 
